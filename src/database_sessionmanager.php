920872c0290fdce9459b9b8bfc85b130f08fd6e7
hichxm <hicham.slimani.fr@gmail.com>
2018-03-01 12:12:28 +0100

    Adding main method of the SessionInterface.

--- src/database_sessionmanager.php
+++ src/database_sessionmanager.php
@@ -4,6 +4,65 @@ namespace Hichxm\SessionManager\Session;
 
 use SessionInterface;
 
+/**
+ * Class DATABASE_SessionManager
+ * @package Hichxm\SessionManager\Session
+ */
 class DATABASE_SessionManager implements SessionInterface {
 
+    /**
+     * SessionInterface constructor.
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * Stop current session.
+     * @return void
+     */
+    public function stop()
+    {
+        // TODO: Implement stop() method.
+    }
+
+    /**
+     * Start session.
+     * @return void
+     */
+    public function start()
+    {
+        // TODO: Implement start() method.
+    }
+
+    /**
+     * Get value of $key
+     * @param string $key
+     * @return string|array|int|boolean|null
+     */
+    public function get($key)
+    {
+        // TODO: Implement get() method.
+    }
+
+    /**
+     * Set value of $key
+     * @param string $key
+     * @param string|array|int|boolean $value
+     * @return void
+     */
+    public function set($key, $value)
+    {
+        // TODO: Implement set() method.
+    }
+
+    /**
+     * Delete value of $key
+     * @param string $key
+     * @return void
+     */
+    public function unset($key)
+    {
+        // TODO: Implement unset() method.
+    }
 }
