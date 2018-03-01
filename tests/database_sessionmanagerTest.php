7a1a384463061504d52fd56cd31970aab2d9f848
hichxm <hicham.slimani.fr@gmail.com>
2018-03-01 13:01:54 +0100

    first test.

--- tests/database_sessionmanagerTest.php
+++ tests/database_sessionmanagerTest.php
@@ -29,5 +29,31 @@ class database_sessionmanagerTest extends \PHPUnit\Framework\TestCase {
         ");
     }
 
+    /**
+     * @test
+     */
+    public function check_if_work_with_start_default_option()
+    {
+        $session_method = new DATABASE_SessionManager([
+            "type" => "mysql",
+            "name" => "sessionmanager",
+            "server" => "127.0.0.1",
+            "port" => 3306,
+            "username" => "root",
+            "password" => ""
+        ]);
+        $session = new SessionManager($session_method);
+
+        $session->start();
+        $session->stop();
+
+        //Delete table
+        $session->bridge->database->query("
+            DROP TABLE `sessionmanager`;
+        ");
+
+        $this->assertTrue(true);
+    }
+
 
 }
