15b9ee37c0b74882a74adc604f52c0932f0735e7
hichxm <hicham.slimani.fr@gmail.com>
2018-03-01 12:53:35 +0100

    make test and delete table.

--- tests/database_sessionmanagerTest.php
+++ tests/database_sessionmanagerTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use Hichxm\SessionManager\Session\DATABASE_SessionManager;
+use Hichxm\SessionManager\SessionManager;
 
 /**
  * Class database_sessionmanagerTest
@@ -10,6 +11,23 @@ class database_sessionmanagerTest extends \PHPUnit\Framework\TestCase {
     /**
      * @test
      */
-    
+    public function check_if_work_with_default_option()
+    {
+        $session_method = new DATABASE_SessionManager([
+            "type" => "mysql",
+            "name" => "sessionmanager",
+            "server" => "127.0.0.1",
+            "port" => 3306,
+            "username" => "root",
+            "password" => ""
+        ]);
+        $session = new SessionManager($session_method);
+
+        //Delete table
+        $session->bridge->database->query("
+            DROP TABLE `sessionmanager`;
+        ");
+    }
+
 
 }
