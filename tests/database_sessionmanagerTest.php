8a51d4acce13c16c93259efbc33a1b614bb3a872
hichxm <hicham.slimani.fr@gmail.com>
2018-03-01 12:11:52 +0100

    class for test

---
+++ tests/database_sessionmanagerTest.php
@@ -0,0 +1,15 @@
+<?php
+
+use Hichxm\SessionManager\Session\DATABASE_SessionManager;
+
+/**
+ * Class database_sessionmanagerTest
+ */
+class database_sessionmanagerTest extends \PHPUnit\Framework\TestCase {
+
+    /**
+     * @test
+     */
+    
+
+}
